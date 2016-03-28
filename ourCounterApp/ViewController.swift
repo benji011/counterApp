4d5e885f580fcaeb37facfef36579d59c25b6744
Benji <wolfnun011@gmail.com>
2016-03-28 20:45:14 +0900

    new feature: checks every possible combinations of letters A, B & C

--- ourCounterApp/ViewController.swift
+++ ourCounterApp/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    // print every combination of ABC
+    @IBOutlet weak  var _comboTextView: UITextView!
+    @IBOutlet       var _outputLabel: UILabel!
+                    var _currentCount = 0
+    
+    let combinationsArray: [String] = ["A", "B", "C"]
+    var outputCombinationsArray = Array<String>()
+    
+    /**
+     * Initialization
+     */
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        printCombinations()
+    }
+    
+    
+    /**
+     * Print every combination of ABC
+     */
+    func printCombinations()
+    {
+        var str: String = ""
+        for letter in combinationsArray {
+            for nextLetter in combinationsArray {
+                for nextNextLetter in combinationsArray {
+                    str += (letter + nextLetter + nextNextLetter + "\n")
+                }
+            }
+        }
+        _comboTextView.text = str
     }
 
-    override func didReceiveMemoryWarning() {
+    
+    /**
+     * Memory warning
+     */
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    /**
+     * Start counting
+     */
+    @IBAction func addOneButton(sender: UIButton)
+    {
+        _currentCount++
+        _outputLabel.text = "クリック回数は：\(_currentCount)"
+        _outputLabel.textColor = UIColor.redColor()
+    }
 }
 
